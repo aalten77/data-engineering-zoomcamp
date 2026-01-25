2714f9fa847663163e08592043ee3a202c65a836
Ai-Linh Alten <32887018+aalten77@users.noreply.github.com>
2026-01-25 17:04:32 +0000

    pipeline code working

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -5,59 +5,56 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
 
 
-def run():
-    pg_user = "root"
-    pg_pass = "root" 
-    pg_host = "localhost"
-    pg_port = 5432
-    pg_db = "ny_taxi"
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
 
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-
-    chunksize=100000
-
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
     
-    parse_dates = [
-        "tpep_pickup_datetime",
-        "tpep_dropoff_datetime"
-    ]
-
-
-    dtype = {
-        "VendorID": "Int64",
-        "passenger_count": "Int64",
-        "trip_distance": "float64",
-        "RatecodeID": "Int64",
-        "store_and_fwd_flag": "string",
-        "PULocationID": "Int64",
-        "DOLocationID": "Int64",
-        "payment_type": "Int64",
-        "fare_amount": "float64",
-        "extra": "float64",
-        "mta_tax": "float64",
-        "tip_amount": "float64",
-        "tolls_amount": "float64",
-        "improvement_surcharge": "float64",
-        "total_amount": "float64",
-        "congestion_surcharge": "float64"
-    }
-
-    engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
     
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     df = pd.read_csv(prefix + f'yellow_tripdata_{year}-{month:02d}.csv.gz')
 
     df_iter = pd.read_csv(
-    prefix + f'yellow_tripdata_{year}-{month:02d}.csv.gz',
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=chunksize 
+        prefix + f'yellow_tripdata_{year}-{month:02d}.csv.gz',
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize 
     )
 
     first = True
